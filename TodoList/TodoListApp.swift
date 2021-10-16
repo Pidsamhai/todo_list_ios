35aba7c1a4885163f3df3f71f66ee77e3a150043
mengxyz <meng348@gmail.com>
2021-10-16 14:26:22 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Anantasak on 16/10/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
